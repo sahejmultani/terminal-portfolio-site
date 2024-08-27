c2e530a8ffa819a1bc90b151df344222a637933c
sahejmultani <smultan7@uwo.ca>
2024-08-26 20:20:17 -0400

    updated about me

--- src/components/commands/About.tsx
+++ src/components/commands/About.tsx
@@ -11,13 +11,21 @@ const About: React.FC = () => {
         Hi, my name is <HighlightSpan>Sahej Multani</HighlightSpan>!
       </p>
       <p>
-        I'm <HighlightAlt>a Computer Software Engineering</HighlightAlt> student at the University of Western Ontario
-        graduating in the class of 2025 .
+        I'm a<HighlightAlt> Computer Software Engineering</HighlightAlt> student at the University of Western Ontario
+        graduating in the class of 2025.
       </p>
       <p>
-        I am passionate about writing codes and <br />
-        developing web applications to solve real-life challenges.
-      </p>
+        I am a skilled software engineer, with most of my experience falling in full-stack development but <br />
+        also am highly proficient in embedded systems development and data engineering. From my academic and <br />
+        professional experiences, I bring a great understanding in many aspects such as end-to-end software <br />
+        development, agile methodologies, technology, and more. Combined with a resiliancy and passion to be <br />
+        as effective as possible and an eager learner, I will showcase my talents and become an effective<br />
+        member of any team!<br /> 
+
+        <br />
+        In my free time I enjoy playing basketball and volleyball and recently started getting more into cultural <br />
+        culturual dancing (bhangra). If you ever need an extra player on your team I'll be there and guarantee a win lol
+        </p>
     </AboutWrapper>
   );
 };
