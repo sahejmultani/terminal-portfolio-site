9c8a5e824ad77c49eb3637e284860f2088bc5cff
sahejmultani <smultan7@uwo.ca>
2024-08-27 20:02:33 -0400

    issue with gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/terminal-portfoio-site/',
   plugins: [
     react(),
     VitePWA({
