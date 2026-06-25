3e62b80e30b2dc0dad80f4f51c76d8680b1804b5
sahejmultani <smultan7@uwo.ca>
2026-06-25 19:07:56 -0400

    fixed hyperlink issue with resume

--- src/components/Output.tsx
+++ src/components/Output.tsx
@@ -11,6 +11,7 @@ import History from "./commands/History";
 import Projects from "./commands/Projects";
 import Socials from "./commands/Socials";
 import Themes from "./commands/Themes";
+import Resume from "./commands/Resume";
 import { OutputContainer, UsageDiv } from "./styles/Output.styled";
 import { termContext } from "./Terminal";
 import { useContext } from "react";
@@ -48,6 +49,7 @@ const Output: React.FC<Props> = ({ index, cmd }) => {
           themes: <Themes />,
           welcome: <Welcome />,
           whoami: <GeneralOutput>visitor</GeneralOutput>,
+          resume: <Resume />,
         }[cmd]
       }
     </OutputContainer>
--- src/components/commands/Resume.tsx
+++ src/components/commands/Resume.tsx
@@ -1,9 +1,15 @@
 export const viewResume = () => {
-  const fileURL = "https://drive.google.com/file/d/1BGLxRGDydQkaFaLh5IwdYwijp9dYhyEP/view";
+  const fileURL =
+    "https://drive.google.com/file/d/1BGLxRGDydQkaFaLh5IwdYwijp9dYhyEP/view";
 
-  console.log("Attempting to open:", fileURL);
-
-  window.open(fileURL, "_blank", "noopener,noreferrer");
-
-  return ("Resume Link: " + fileURL);
+  return (
+    <span>
+      Here's a link to my resume!{" "}
+      <a href={fileURL} target="_blank" rel="noopener noreferrer">
+        {fileURL}
+      </a>
+    </span>
+  );
 };
+
+export default viewResume;
