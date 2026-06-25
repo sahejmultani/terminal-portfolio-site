d06fa1ead1b15a57f7242aefd8ea5dca5aee783f
sahejmultani <smultan7@uwo.ca>
2026-06-25 18:41:22 -0400

    updated resume link

--- src/components/commands/Resume.tsx
+++ src/components/commands/Resume.tsx
@@ -1,5 +1,5 @@
 export const viewResume = () => {
-  const fileURL = "https://drive.google.com/file/d/1saujzosrQ3xcldRgv5bayPz0h97jbPJC/view?usp=sharing"; // Ensure this path is correct
+  const fileURL = "https://drive.google.com/file/d/1BGLxRGDydQkaFaLh5IwdYwijp9dYhyEP/view"; // Ensure this path is correct
 
   // Open the URL in a new tab
   window.open(fileURL, "_blank");
